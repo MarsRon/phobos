b46e80292431576f785e7c4ee2468d71abb141fe
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-03-23 12:24:52 +0800

    update word catcher

--- features/word-catcher.js
+++ features/word-catcher.js
@@ -1,24 +1,25 @@
-function imDad(message) {
-	const { channel, content } = message;
-	const match = /^i\s*('|a)?\s*m\s*/gi.exec(content);
-	if (match) {
-		const text = content.slice(match[0].length).trim();
-		channel.send(`Hi **${text === "" ? "blank" : text}**, I'm dad!`);
-	}
-}
+const catchers = [
+	async function imDad(message) {
+		const { channel, content } = message;
+		const match = /^i\s*('|a)?\s*m\s*/gi.exec(content);
+		if (match) {
+			const text = content.slice(match[0].length).trim();
+			channel.send(`Hi **${text === "" ? "blank" : text}**, I'm dad!`);
+		}
+	},
 
-async function brrr(message) {
-	if (message.content.includes("brrr")) {
-		await message.react("🏎️");
-		await message.react("🇻");
-		await message.react("🇷");
-		await message.react("🇴");
-		await message.react("🅾️");
-		message.react("🇲");
+	async function brrr(message) {
+		if (message.content.toLowerCase().includes("brrr")) {
+			await message.react("🏎️");
+			await message.react("🇻");
+			await message.react("🇷");
+			await message.react("🇴");
+			await message.react("🅾️");
+			message.react("🇲");
+		}
 	}
-}
+];
 
 module.exports = function(message) {
-	imDad(message);
-	brrr(message);
+	catchers.forEach(func => func(message));
 }
