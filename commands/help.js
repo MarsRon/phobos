fca29d4b5cec8e81ac554b5590be9bb55688df66
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-03-26 00:15:30 +0800

    Help command list | .replit auto install packages

--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,18 @@ module.exports = {
 		const { channel, client } = message;
 
 		if (args.length) {
+			if (args[0] === "cmds") {
+				return channel.send(new MessageEmbed()
+					.setAuthor("Command List", "https://cdn.discordapp.com/avatars/738252807525892139/70c554767b079e2774ea9a7d8b432cb7.webp?size=32", "https://marsron.github.io")
+					.setColor("#2AA1FF")
+					.addFields(Array.from(client.commands)
+						.map(([name, { description: value }]) =>
+							({ name: prefix + name, value, inline: true })
+						)
+					)
+				);
+			}
+			
 			const getCmd = cmdName => {
 				const cmd = client.commands.get(cmdName);
 				if (cmd) return cmd;
@@ -34,6 +46,7 @@ module.exports = {
 		channel.send({embed: {
 			color: 2793983,
 			fields: [
+				{ name: ":open_file_folder: Commands List", value: `\`${prefix}help cmds\``},
 				{ name: ":white_check_mark: Help Page", value: "https://marsron.github.io" },
 				{ name: ":question: Arguments usage", value: "`<required>`, `[optional]`" },
 				{ name: ":page_facing_up: Still need help?", value: "[Click here](https://discord.gg/TSqw3jx) to join our Discord server" }
