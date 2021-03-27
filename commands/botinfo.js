de579f041d7769400365a1f86ce01f88f59ea258
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-03-27 15:45:34 +0800

    Embed messages

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,6 +2,14 @@ module.exports = {
 	name: "botinfo",
 	description: "Information about the bot",
 	execute(message) {
-		message.reply(`Bot was created by MarsRon, the <:cri:774494797871972403> champion.\nInvite me with \`${process.env.PREFIX}invite\`!`)
+		message.reply({embed: {
+			description: `Phobos was created by <@611166639534112769>, the <:cri:774494797871972403> champion.\n**[Click here](https://discord.com/oauth2/authorize?client_id=738252807525892139&scope=bot&permissions=2147483647)** to invite me to your server!`,
+			color: 2793983,
+			author: {
+				name: "Bot Information",
+				url: "https://marsron.github.io",
+				icon_url: "https://cdn.discordapp.com/avatars/738252807525892139/70c554767b079e2774ea9a7d8b432cb7.webp?size=32"
+			}
+		}});
 	}
 }
--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,18 @@ module.exports = {
 	name: "ping",
 	description: "Returns bot latency and passed arguments",
 	execute(message, args) {
-		message.reply(`:ping_pong: Pong!\nBot Latency: **${Date.now() - message.createdTimestamp}ms**\nArguments passed: ${args.length ? `\`${args.join("`, `")}\`` : "**None**"}`);
+		message.reply({embed: {
+			title: ":ping_pong: Pong!",
+			fields: [
+				{ name: "Bot Latency", value: `${Date.now() - message.createdTimestamp}ms`, inline: true },
+				{ name: "Arguments passed", value: args.length ? `\`${args.join("`, `")}\`` : "None", inline: true }
+			],
+			color: 2793983,
+			author: {
+				name: "Phobos",
+				url: "https://marsron.github.io",
+				icon_url: "https://cdn.discordapp.com/avatars/738252807525892139/70c554767b079e2774ea9a7d8b432cb7.webp?size=32"
+			}
+		}});
 	}
 }
