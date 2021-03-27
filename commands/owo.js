b5c13cdcde37d4baf3ca378fddca88919d801d15
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-03-27 19:26:15 +0800

    Add OwOfire to projects

---
+++ commands/owo.js
@@ -0,0 +1,12 @@
+const owo = require("owofire");
+
+module.exports = {
+	name: "owo",
+	description: "Owoify your message",
+	args: true,
+	usage: "<text>",
+	execute(message, args) {
+		message.reply(owo(args.join(" ")))
+			.then(msg => msg.react("745664423456145424").catch(() => {}));
+	}
+}
