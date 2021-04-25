b1dc3ac6feb4568d9ec4c958dc743b614a0afa07
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-04-25 15:05:04 +0800

    Lyrics command

---
+++ commands/music/lyrics.js
@@ -0,0 +1,56 @@
+const axios = require("axios").default;
+
+module.exports = {
+	name: "lyrics",
+	description: "Finds the lyrics of the current song or a provided song name.",
+	usage: "<link|query>",
+	execute(message, args) {
+		let query = args.join(" ");
+		const { client, guild, member, author: user } = message;
+
+		if (guild) {
+			const queue = client.distube.getQueue(message);
+			if (queue)
+				query = queue.songs[0].title;
+		}
+
+		if (query === "")
+			return message.reply(":x: You need to provide a song name to be searched");
+		message.reply(`:mag_right: **Searching lyrics for** \`${query}\``);
+
+		axios.get("https://some-random-api.ml/lyrics?title=" + encodeURIComponent(query))
+			.then(({ data: { author, links, lyrics, title, thumbnail } }) => {
+				const splitLyrics = lyrics.split("\n");
+				let maxI = 0, count = 0, overflowed = false;
+				for (const line of splitLyrics) {
+					let sum = count + line.length + 1;
+					if (sum > 2044) {
+						overflowed = true;
+						break;
+					}
+					count = sum;
+					maxI++;
+				}
+				lyrics = splitLyrics.filter((_, i) => i < maxI).join("\n");
+				if (overflowed) lyrics += "\n...";
+				message.channel.send(typeof data === "string" ?
+					":x: Lyrics not found" :
+					{embed: {
+						title: `${author} - ${title}`,
+						description: lyrics,
+						thumbnail: { url: Object.values(thumbnail)[0] },
+						color: 2793983,
+						url: Object.values(links)[0],
+						footer: {
+							text: `Requested by ${member?.displayName || user.username}`,
+							icon_url: user.displayAvatarURL({ dynamic: true, size: 32 })
+						}
+					}}
+				);
+			})
+			.catch(err => {
+				console.log(err);
+				message.reply(":x: An error occured: " + err.response?.data.error);
+			});
+	}
+};
