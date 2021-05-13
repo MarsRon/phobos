9c0c838466c419e87b0bcaab65cf22df014c520f
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-13 16:07:49 +0800

    Serve directory fix

--- src/handlers/server.js
+++ src/handlers/server.js
@@ -5,6 +5,6 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("site"));
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use("/assets", express.static(path.join(__dirname, "../../assets")));
 
 app.listen(port, () => console.log(`Website running on port ${port}`));
