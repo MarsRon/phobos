07adcd6f20bb4d4151f5bba61e4ab7fdaef63e98
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-15 18:29:33 +0800

    Update shuffle command

--- src/commands/music/shuffle.js
+++ src/commands/music/shuffle.js
@@ -1,50 +1,16 @@
 const Guild = require("../../db/guild");
 
-function shuffle(array) {
-	let l = array.length, t, i;
-	while (l) {
-		i = Math.floor(Math.random() * l--);
-		t = array[l];
-		array[l] = array[i];
-		array[i] = t;
-	}
-	return array;
-}
-
 module.exports = {
 	name: "shuffle",
 	description: "Shuffles the music queue.",
 	guildOnly: true,
 	async execute(message) {
-		const { author, client, guild } = message;
-		const queue = client.distube.getQueue(message);
-		const gdb = await Guild(guild.id);
+		const { client: { distube }, guild } = message;
+		const { prefix } = (await Guild(guild.id)).get();
+		const queue = distube.getQueue(message);
 		if (!queue)
-			return message.reply(`:x: **I am not playing music. Use** \`${gdb.get().prefix}play\`** to play some music!**`);
-		const { songs } = queue;
-		const currentlyPlaying = songs.shift();
-		shuffle(songs);
-		songs.unshift(currentlyPlaying);
-		const formattedSongs = queue.songs.map(({ name, url, formattedDuration, user }, index) => `\`${index}.\` [${name}](${url}) | \`${formattedDuration}\` Requested by: <@${user.id}> (${user.tag})`);
-		let maxI = 0, count = 0;
-		for (const song of formattedSongs) {
-			let sum = count + song.length + 1;
-			if (sum > 2031)
-				break;
-			count = sum;
-			maxI++;
-		}
-		message.reply({
-			content: "Queue shuffled",
-			embed: {
-				title: `Queue for ${guild.name}`,
-				description: `__Now Playing__:\n${formattedSongs.filter((_, i) => i < maxI).join("\n")}`,
-				color: 4404979,
-				footer: {
-					text: `Volume: ${queue.volume}% | Filter: ${queue.filter || "❌"} | Loop: ${queue.repeatMode ? queue.repeatMode == 2 ? "Entire Queue" : "This Song" : "❌"} | Autoplay: ${queue.autoplay ? "On" : "❌"}`,
-					icon_url: author.displayAvatarURL({ dynamic: true })
-				}
-			}
-		});
+			return message.reply(`:x: **I am not playing music. Use** \`${prefix}play\`** to play some music!**`);
+		distube.shuffle(message);
+		message.reply(`Queue shuffled. Use \`${prefix}queue\` to view the music queue!`);
 	}
 };
