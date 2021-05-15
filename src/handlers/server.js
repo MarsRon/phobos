091f55eb7fbe9a64ec1b64e87a8c995945be7cee
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-15 17:36:57 +0800

    Fix express not serving logo.svg

--- src/handlers/server.js
+++ src/handlers/server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("site"));
-app.use("/assets", express.static(path.join(__dirname, "../../assets")));
+app.use("/assets", express.static("assets"));
 
 app.listen(port, () => console.log(`Website running on port ${port}`));
