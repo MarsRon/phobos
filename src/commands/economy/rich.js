3fc01c10f97d1ce7a136c633ea24e847a7987880
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-17 21:36:52 +0800

    Rich command bugfix

--- src/commands/economy/rich.js
+++ src/commands/economy/rich.js
@@ -7,15 +7,21 @@ module.exports = {
 	execute(message) {
 		const { client: { users } } = message;
 		Promise.all([
-			message.reply(":mag_right: Fetching the top 10 richest users..."),
-			model.find({}, null, { sort: { coins: -1 }, limit: 10 }).exec()
+			message.reply(":mag_right: Fetching the Top 10 Richest Users Of Phobos..."),
+			model.aggregate([
+				{ $project: {
+					userID: 1, coins: 1, bank: 1, sum: { $add: ["$coins", "$bank"] }
+				} },
+				{ $sort: { sum: -1 } },
+				{ $limit: 10 }
+			])
 		]).then(async ([msg, res]) => {
 			const data = [];
 			const emoji = [":first_place:", ":second_place:", ":third_place:"];
 			for (let i = 0; i < res.length; i++) {
-				const { bank, coins, userID } = res[i];
+				const { sum, userID } = res[i];
 				const user = users.cache.get(userID) ?? await users.fetch(userID);
-				data.push(`${emoji[i] ?? "🔹"} **${(coins + bank).toLocaleString()}$** - ${user.tag}`);
+				data.push(`${emoji[i] ?? "🔹"} **${sum.toLocaleString()}$** - ${user.tag}`);
 			}
 			msg.edit({
 				content: null,
@@ -23,11 +29,11 @@ module.exports = {
 					description: data.join("\n"),
 					color: 4404979,
 					author: {
-						name: "Top 10 richest users of Phobos",
+						name: "Top 10 Richest Users Of Phobos",
 						url: "https://phobos.marsron.repl.co",
 						icon_url: "https://cdn.discordapp.com/avatars/738252807525892139/3d8cd9c0887eeb2c8b6b4a6226e3710a.webp?size=32"
 					},
-					footer: { text: "NOTE: This is COINs + BANKs, not net worth" }
+					footer: { text: "NOTE: This doesn't include inventory items." }
 				}
 			});
 		}).catch(e => {
