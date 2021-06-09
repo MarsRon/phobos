3276c8d54505e974c661f87d1a738d83e870d8b4
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-06-09 14:00:23 +0800

    perf: remove unnecessary code 🚀

--- src/handlers/inlineReply.js
+++ src/handlers/inlineReply.js
@@ -2,16 +2,13 @@ const { APIMessage, Structures } = require("discord.js");
 
 class Message extends Structures.get("Message") {
 	async reply(content, options) {
-		const mentionRepliedUser = typeof ((options || content || {}).allowedMentions || {}).repliedUser === "undefined" ? false : ((options || content).allowedMentions).repliedUser;
-		delete((options || content || {}).allowedMentions || {}).repliedUser;
-
 		const apiMessage = content instanceof APIMessage ? content.resolveData() : APIMessage.create(this.channel, content, options).resolveData();
 		Object.assign(apiMessage.data, { message_reference: { message_id: this.id } });
 
 		if (!apiMessage.data.allowed_mentions || Object.keys(apiMessage.data.allowed_mentions).length === 0)
 			apiMessage.data.allowed_mentions = { parse: ["users", "roles", "everyone"] };
-		if (typeof apiMessage.data.allowed_mentions.replied_user === "undefined")
-			Object.assign(apiMessage.data.allowed_mentions, { replied_user: mentionRepliedUser });
+
+		Object.assign(apiMessage.data.allowed_mentions, { replied_user: false });
 
 		if (Array.isArray(apiMessage.data.content)) {
 			return Promise.all(apiMessage.split().map(x => {
