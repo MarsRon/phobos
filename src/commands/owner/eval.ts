b02bb6f890401581124577c1877f084108a9d764
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-06-18 19:13:40 +0800

    feat: improve eval command ✨

--- src/commands/owner/eval.ts
+++ src/commands/owner/eval.ts
@@ -1,36 +1,60 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-eval */
-
 import { Message } from 'discord.js'
 import { inspect } from 'util'
 
-const ownerID = process.env.OWNER_ID
+const { OWNER_ID: ownerID, PREFIX: prefix } = process.env
 
-const clean = (text: any) =>
-  typeof (text) === 'string' ? text.replace(/`/g, '`\u200b') : text
+async function discordEval (code: string, message: Message) {
+  if (!code) return
 
-module.exports = {
+  if (code.startsWith('```')) {
+    code = code.replace(/^```(?:js)?([\S\s]+)```$/is, '$1').trim()
+  }
+
+  let result: any
+
+  const send = () => message.reply({
+    embed: {
+      description: '```js\n' + inspect(result, true, 0).slice(0, 2048 - 9) + '```',
+      fields: [
+        ['Class', `\`${result !== undefined && result !== null ? result.constructor.name : 'void'}\``],
+        ['Type', `\`${typeof result}\``]
+      ].map(([name, value]) => ({ name, value, inline: true })),
+      color: 0x4336F3,
+      author: {
+        name: 'Eval Result',
+        url: 'https://phobos.marsron.repl.co',
+        icon_url: 'https://cdn.discordapp.com/avatars/738252807525892139/3d8cd9c0887eeb2c8b6b4a6226e3710a.webp?size=32'
+      }
+    }
+  })
+
+  try {
+    // Inject variables
+    // eslint-disable-next-line no-unused-vars
+    const { channel, client, content, guild, member, author: user } = message
+
+    if (code.includes('await')) {
+      result = await (eval(`async () => {${code}}`)() as Promise<any>)
+    } else {
+      result = eval(code)
+    }
+  } catch (err: any) {
+    result = err.message ?? err
+  }
+
+  send()
+}
+
+export default {
   name: 'eval',
   description: 'Run JavaScript code.',
   args: true,
   usage: '<js_code>',
-  async execute (message: Message, args: string[]) {
-    const { channel, client, guild, member, author: user } = message
-
-    if (user.id !== ownerID) return
-    if (!args.length) return
-
-    const OUT = (text: any) =>
-      channel.send(clean(text), { code: 'js', split: true })
-
-    try {
-      let evaled = eval(args.join(' '))
-      if (typeof (evaled) !== 'string') {
-        evaled = inspect(evaled)
-      }
-      message.reply(clean(evaled), { code: 'js', split: true })
-    } catch (err: any) {
-      message.reply(clean(err.message), { code: 'js', split: true })
-    }
+  async execute (message: Message) {
+    const { author, content } = message
+    if (author.id !== ownerID) return
+    const code = content.slice(prefix!.length + 4).trim()
+    discordEval(code, message)
   }
 }
