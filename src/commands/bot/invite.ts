84b97029aa3e2e34d4dbca4333f6c30fe06b3d6f
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-07-01 11:14:44 +0800

    feat: add invite command ✨

---
+++ src/commands/bot/invite.ts
@@ -0,0 +1,25 @@
+import { Message } from 'discord.js'
+import config from '../../config'
+
+const { embed: { avatar, color, url }, invite } = config
+
+const inviteEmbed = {
+  embed: {
+    description: `[**Add me**](${invite}) to your server! 🎉`,
+    color,
+    author: {
+      name: 'Invite Phobos',
+      url,
+      icon_url: avatar
+    },
+    footer: { text: "It's easy, fast, free & non-regrettable." }
+  }
+}
+
+export default {
+  name: 'invite',
+  description: 'Add me to your server! 🎉',
+  execute (message: Message) {
+    message.reply(inviteEmbed)
+  }
+}
