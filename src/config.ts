1e78849932d3a21a2e8cbe6306173304d24df45d
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-07-02 19:22:39 +0800

    docs: add config docs 📚

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,15 @@
+import { ActivityOptions } from 'discord.js'
+import { PhobosClient } from './handlers/client'
+
 // Types
 interface PhobosEmbedConfig {
   avatar: string
   color: number
   url: string
+}
+interface PhobosActivity {
+  name: (client: PhobosClient) => string
+  options: ActivityOptions
 }
 interface PhobosConfig {
   prefix: string
@@ -10,22 +17,64 @@ interface PhobosConfig {
   invite: string
   supportServer: string
   embed: PhobosEmbedConfig
-}
-
-// Change the embed configurations here!
-export const embed: PhobosEmbedConfig = {
-  avatar: 'https://cdn.discordapp.com/avatars/738252807525892139/3d8cd9c0887eeb2c8b6b4a6226e3710a.webp',
-  color: 0x4336F3,
-  url: 'https://phobos.marsron.repl.co'
+  activity: PhobosActivity
 }
 
 // Change the bot configurations here!
+
 const config: PhobosConfig = {
+
+  // Command prefix (e.g. !help, ?help, >help, .help etc.)
   prefix: '.',
+
+  // YOUR Discord user ID
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // NOTE: DO NOT PUT IDS OF ANYONE EXCEPT YOURSELF
+  // BECAUSE THIS WILL ALLOW THEM TO USE THE EVAL COMMAND
+  // THIS IS VERY DANGEROUS PLEASE KEEP THAT IN MIND
+  // FOR MORE INFORMATION PLEASE READ:
+  // https://anidiots.guide/examples/making-an-eval-command#securing-your-eval
+  // Or you can just remove the eval command ;)
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   ownerID: '611166639534112769',
+
+  // Bot invite
   invite: 'https://discord.com/oauth2/authorize?client_id=738252807525892139&scope=bot&permissions=8589934591',
+
+  // Discord support server invite link, or just your personal discord server
   supportServer: 'https://discord.gg/TSqw3jx',
-  embed
+
+  // Embed configurations
+  embed: {
+
+    // Image of the embeds
+    avatar: 'https://cdn.discordapp.com/avatars/738252807525892139/3d8cd9c0887eeb2c8b6b4a6226e3710a.webp',
+
+    // Color of the embeds (Number)
+    color: 0x4336F3,
+
+    // URL of the embeds
+    url: 'https://phobos.marsron.repl.co'
+
+  } as PhobosEmbedConfig,
+
+  // Bot activity (optional)
+  // For more info please read:
+  // https://discord.js.org/#/docs/main/stable/class/ClientUser?scrollTo=setActivity
+  activity: {
+
+    // Bot activity string (optional)
+    // This is a function that returns a string
+    name: (client: PhobosClient): string =>
+      `${config.prefix}help | ${client.guilds.cache.size} Servers`,
+
+    // Bot activity options (optional)
+    // All available options here:
+    // https://discord.js.org/#/docs/main/stable/typedef/ActivityOptions
+    options: { type: 'PLAYING' }
+
+  }
+
 }
 
 export default config
