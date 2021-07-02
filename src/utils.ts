e52ffe005e1d9d90c2f81512f8c8d5ae7cd5727a
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-07-02 20:23:34 +0800

    feat: add get(User|Member)FromMessage utils ✨

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { GuildMember, Message, User } from 'discord.js'
+
 /**
  * Format duration seconds into DHMS '1d 2h 3m 4s' string
  * @param {number} seconds - Duration in seconds
@@ -28,4 +30,57 @@ export function timeToStr (seconds: number): string {
   }
   str += `${seconds.toFixed(0)} second${seconds !== 1 ? 's' : ''}`
   return str
+}
+
+/**
+ * Get a user from a Message's mentions
+ * @param {Message} message - Discord message
+ * @param {string} idQuery - ID query
+ * @returns {Promise<User>}
+ */
+export async function getUserFromMessage (
+  message: Message, idQuery: string
+): Promise<User> {
+  const { author, guild, mentions } = message
+
+  const userMention = mentions.users.first()
+  if (userMention) {
+    return userMention
+  }
+
+  if (guild) {
+    const id = idQuery.match(/\d+/)?.[0]
+    if (id) {
+      const member = await guild.members.fetch(id)
+      return member.user
+    }
+  }
+
+  return author
+}
+
+/**
+ * Get a member from a Message's mentions
+ * @param {Message} message - Discord message
+ * @param {string} idQuery - ID query
+ * @returns {Promise<GuildMember | null>}
+ */
+export async function getMemberFromMessage (
+  message: Message, idQuery: string
+): Promise<GuildMember | null> {
+  const { member, guild, mentions } = message
+
+  const memberMention = mentions.members?.first()
+  if (memberMention) {
+    return memberMention
+  }
+
+  if (guild) {
+    const id = idQuery.match(/\d+/)?.[0]
+    if (id) {
+      return await guild.members.fetch(id)
+    }
+  }
+
+  return member
 }
