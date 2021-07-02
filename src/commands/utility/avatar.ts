0a873cb7b7753eb62b71e5405f92b4244404ca15
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-07-02 20:53:23 +0800

    feat: add avatar command ✨

---
+++ src/commands/utility/avatar.ts
@@ -0,0 +1,31 @@
+import { Message } from 'discord.js'
+import { getUserFromMessage } from '../../utils'
+import config from '../../config'
+
+const { embed: { color } } = config
+
+export default {
+  name: 'avatar',
+  alias: ['av', 'pfp'],
+  description: 'Fetches the avatar/profile picture of a user.',
+  usage: '[user]',
+  async execute (message: Message, args: string[]) {
+    const user = await getUserFromMessage(message, args[0])
+
+    const url = user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 })
+
+    message.reply({
+      embed: {
+        title: 'Avatar',
+        url,
+        color,
+        author: {
+          name: user.tag,
+          url,
+          icon_url: url
+        },
+        image: { url }
+      }
+    })
+  }
+}
