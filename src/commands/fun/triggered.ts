77580f2c93d6797bf2c2ac8a079b1a60289a6b21
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-07-09 12:15:38 +0800

    feat: add triggered command ✨

---
+++ src/commands/fun/triggered.ts
@@ -0,0 +1,24 @@
+import { Message, MessageAttachment } from 'discord.js'
+import { getUserFromMessage } from '../../utils'
+import axios from 'axios'
+
+export default {
+  name: 'triggered',
+  description: "I'm triggered. >:(\n*Wait for a few seconds it's pretty slow*",
+  usage: '[user]',
+  cooldown: 5,
+  async execute (message: Message, args: string[]) {
+    const user = await getUserFromMessage(message, args.shift())
+    const avatar = user.displayAvatarURL({ format: 'png', size: 256 })
+    const { data } = await axios.get(
+      'https://some-random-api.ml/canvas/triggered',
+      {
+        responseType: 'arraybuffer',
+        params: { avatar }
+      }
+    )
+    message.reply(
+      new MessageAttachment(data, `${user.username}-triggered.gif`)
+    )
+  }
+}
