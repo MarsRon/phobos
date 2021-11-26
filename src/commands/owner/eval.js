eabd3d09783bb1c58ae2b51b2f394b2abc7616bb
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-11-26 18:49:59 +0800

    Improve eval command function

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -6,62 +6,85 @@ const {
   ownerId
 } = config
 
-async function discordEval (code, message) {
-  if (!code) return
-
-  if (code.startsWith('```')) {
-    code = code.replace(/^```(?:js)?([\S\s]+)```$/is, '$1')
-  }
-
-  const send = result =>
-    message.reply({
-      embeds: [
-        {
-          description:
-            '```js\n' + inspect(result, true, 0).slice(0, 2048 - 9) + '```',
-          fields: [
-            [
-              'Class',
-              `\`${
-                result !== undefined && result !== null
-                  ? result.constructor.name
-                  : 'void'
-              }\``
-            ],
-            ['Type', `\`${typeof result}\``]
-          ].map(([name, value]) => ({ name, value, inline: true })),
-          color,
-          author: { name: 'Eval Result', url, icon_url: avatar }
-        }
-      ]
-    })
-
-  let result
-  try {
-    // Inject variables
-    const { channel, client, content, guild, member, author: user } = message
-    const { db, config } = client
-
-    if (code.includes('await')) {
-      result = await eval(`async () => {${code}}`)()
-    } else {
-      result = eval(code)
-    }
-  } catch (err) {
-    result = err.message ?? err
-  }
-  send(result)
-}
-
 module.exports = {
   name: 'eval',
   description: 'Run JavaScript code.',
   args: true,
   usage: '<js_code>',
   async execute (message) {
-    const { author, content } = message
+    // Owner check
+    const { author, content, prefix } = message
     if (author.id !== ownerId) return
-    const code = content.slice(message.prefix.length + 4).trim()
-    discordEval(code, message)
+
+    // Directly get code from message content
+    let code = content.slice(prefix.length + this.name.length).trim()
+
+    if (!code) return
+
+    // Filter out codeblock
+    if (code.startsWith('```')) {
+      code = code.replace(/^```(?:js)?([\S\s]+)```$/is, '$1')
+    }
+
+    // Get class of object
+    const handleResultClass = result =>
+      result !== undefined && result !== null ? result.constructor.name : 'void'
+
+    // Display objects with inspect but not strings
+    const handleResult = result => {
+      const className = handleResultClass(result)
+      // Parse errors
+      if (className.includes('Error')) {
+        result = `${className}: ${result.message}` ?? result
+      }
+      // Don't parse strings
+      if (typeof result !== 'string') {
+        result = inspect(result, true, 0)
+      }
+      // Limit to 4096 characters
+      if (result.length > 4096 - 9) {
+        result = result.slice(0, 4096 - 12) + '...'
+      }
+      return '```js\n' + result + '```'
+    }
+
+    // Send data as discord message
+    const send = result =>
+      message.reply({
+        embeds: [
+          {
+            description: handleResult(result),
+            fields: [
+              ['Class', `\`${handleResultClass(result)}\``],
+              ['Type', `\`${typeof result}\``]
+            ].map(([name, value]) => ({ name, value, inline: true })),
+            color,
+            author: { name: 'Eval Result', url, icon_url: avatar }
+          }
+        ]
+      })
+
+    // Run eval with variables
+    const runEval = async code => {
+      try {
+        // Inject variables
+        let { channel, client, content, guild, member, author: user } = message
+        let { db, config, distube } = client
+        // Automatically detect async code
+        if (code.includes('await')) {
+          return eval(`async () => {
+            try {${code}}
+            catch (error) {
+              return error
+            }
+          }`)()
+        }
+        return eval(code)
+      } catch (error) {
+        return error
+      }
+    }
+
+    send(await runEval(code))
   }
 }
