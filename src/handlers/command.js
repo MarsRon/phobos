a7eb35dbff1c2958a44c1afbd734f43550432444
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-11-29 17:50:28 +0800

    Improve readability

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,48 +1,48 @@
-// Modules
 const { Collection } = require('discord.js')
-const { readdir } = require('fs')
+const { readdirSync } = require('fs')
 const path = require('path')
 
 const logger = require('./logger')
 
+// Get collections from client
+const {
+  commands: commandsCollection,
+  cooldowns,
+  aliases
+} = require('../client')
+
 // Get all categories
-readdir(path.join(__dirname, '../commands'), (err, commandsFolder) => {
-  if (err) {
-    return logger.error(err)
+const commandsFolder = readdirSync(path.join(__dirname, '../commands'))
+
+for (const category of commandsFolder) {
+  // Set category in collection
+  const categoryCollection = new Collection()
+  commandsCollection.set(category, categoryCollection)
+
+  // Get all commands in this category
+  const categoryPath = path.join(__dirname, '../commands/', category)
+  const categoryFolder = readdirSync(categoryPath)
+
+  // Read from command files
+  const commands = categoryFolder
+    .filter(file => /\.js$/.test(file))
+    .map(file => require(`../commands/${category}/${file}`))
+
+  // Set up each command, alias and cooldown
+  for (const command of commands) {
+    const { name, alias } = command
+
+    // Command
+    categoryCollection.set(name, command)
+    
+    // Alias
+    if (alias) {
+      alias.forEach(a => aliases.set(a, command))
+    }
+    
+    // Cooldown
+    cooldowns.set(name, new Collection())
   }
-  const {
-    commands: commandsCollection,
-    cooldowns,
-    aliases
-  } = require('../client')
-  for (const category of commandsFolder) {
-    const categoryCollection = new Collection()
-    commandsCollection.set(category, categoryCollection)
 
-    // Get all commands in this category
-    const categoryPath = path.join(__dirname, '../commands/', category)
-    readdir(categoryPath, (err, categoryFolder) => {
-      if (err) {
-        return logger.error(err)
-      }
-
-      const commands = categoryFolder
-        .filter(file => /\.js$/.test(file))
-        .map(file => require(`../commands/${category}/${file}`))
-
-      for (const command of commands) {
-        const { name, alias } = command
-
-        categoryCollection.set(name, command)
-        cooldowns.set(name, new Collection())
-        if (alias) {
-          alias.forEach(a => aliases.set(a, command))
-        }
-      }
-
-      logger.debug(
-        `Loaded ${commands.length} commands from category '${category}'`
-      )
-    })
-  }
-})
+  logger.debug(`Loaded ${commands.length} commands from category '${category}'`)
+}
--- src/handlers/server.js
+++ src/handlers/server.js
@@ -1,13 +1,23 @@
+// I'm using nginx as a reverse proxy to proxy port 8001
+// You need to set up your reverse proxy yourself ¯\_(ツ)_/¯
+
 const express = require('express')
-const client = require('../client')
 const logger = require('./logger')
 
 const app = express()
+// In case I ever need to change the port
 const port = process.env.PORT || 8001
 
+// Static serve
 app.use(express.static('site'))
 app.use('/assets', express.static('assets'))
 
+// Redirect 404 requests to root
+app.all('*', (req, res) => {
+  res.redirect('/')
+})
+
+// Start server
 app.listen(port, () =>
   logger.info(`Webserver running on https://localhost:${port}`)
 )
