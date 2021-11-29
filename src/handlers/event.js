8bcafce8aaf8528f805801f18cd6b2ff933c0294
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-11-29 16:42:11 +0800

    screw it im using readdirSync

--- src/handlers/event.js
+++ src/handlers/event.js
@@ -1,21 +1,23 @@
 const client = require('../client')
-const { readdir } = require('fs')
+const { readdirSync } = require('fs')
 const path = require('path')
 
-readdir(path.join(__dirname, '../events'), (err, files) => {
-  if (err) {
-    return client.log.error(`[handler/event] ${err}`)
-  }
-  files
-    .filter(file => file.endsWith('.js'))
-    .map(name => name.slice(0, -3))
-    .forEach(name => {
-      const eventHandler = require(`../events/${name}`)
-      if (eventHandler.once) {
-        client.once(name, eventHandler)
-      } else {
-        client.on(name, eventHandler)
-      }
-      client.log.debug(`Loaded event '${name}'`)
-    })
-})
+const files = readdirSync(path.join(__dirname, '../events'))
+
+files
+  // Filter js files
+  .filter(file => file.endsWith('.js'))
+  // Remove extension
+  .map(name => name.slice(0, -3))
+  .forEach(name => {
+    const eventHandler = require(`../events/${name}`)
+
+    // Run some events only once
+    if (eventHandler.once) {
+      client.once(name, eventHandler)
+    } else {
+      client.on(name, eventHandler)
+    }
+
+    client.log.debug(`Loaded event '${name}'`)
+  })
