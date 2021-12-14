807bc0f025b1ef88061d1dbb259dcc76c96f6d7f
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-14 15:00:55 +0800

    fix error stack not logging

--- src/handlers/logger.js
+++ src/handlers/logger.js
@@ -9,18 +9,17 @@ const log = createLogger({
     }),
     format.colorize({ level: true }),
     format.errors({ stack: true }),
-    format.printf(a => {
-      const { level, message, timestamp } = a
-      const txt = `${timestamp} [${level.replace(
-        /(?<=\x1B\[.+?m).+?(?=\x1B\[.+?m)/,
-        m => m.toUpperCase()
-      )}]: ${
-        typeof message === 'string'
-          ? message
-          : inspect(message, { colors: true })
-      }`
-      return txt
-    })
+    format.printf(
+      ({ level, message, timestamp, stack }) =>
+        `${timestamp} [${level.replace(
+          /(?<=^\x1B\[.+?m).+?(?=\x1B\[.+?m$)/,
+          m => m.toUpperCase()
+        )}] ${
+          stack ?? typeof message === 'string'
+            ? message
+            : inspect(message, { colors: true })
+        }`
+    )
   ),
   transports: [new transports.Console()]
 })
