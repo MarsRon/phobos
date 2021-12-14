552d74b229470be0741ce849fe0da50367a2e450
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-14 13:16:06 +0800

    Update hack command

--- src/commands/fun/hack.js
+++ src/commands/fun/hack.js
@@ -5,55 +5,57 @@ const { getUserFromMessage } = require('../../utils')
 const { color } = config.embed
 
 const domains = [
-  'yousuck.noob',
-  'mail.discord.com',
-  'hesnoob.haha',
-  'thisguy.suck',
-  'paypal.removed',
-  'noob.haha',
-  'hacked.xyz',
-  'susmate.com',
-  'gmail.sus',
-  'why-im.withyou',
-  'someone-end.me',
-  'isnoob.io',
-  'you-are.noob',
-  'hahaget.lost',
-  'yahoo.sus',
-  'botmail.zip',
-  'gmail.com',
-  'yahoo.com'
+  'rickroll.xyz',
+  'nevergonnagiveyouup.lol',
+  'superidol.xiaorong',
+  'bobux.free',
+  'get.free.vbux',
+  'free-mc.net',
+  'sussy.baka.sus',
+  'jenshin.impact',
+  'get-rekt.haha',
+  'urnoob.boo',
+  'amogus.sus',
+  'roblox.guest',
+  'deez.nuts',
+  'ur-getting.ddos',
+  'yousuck.cringe',
+  'getlostya.bum'
 ]
 
 const passwords = [
-  'Disb\\*\\*\\*\\*',
-  'mdsb\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*',
-  'disc\\*\\*\\*\\*\\*\\*',
-  'pass\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*',
-  'get\\*\\*\\*\\*',
-  'mails\\*\\*\\*',
-  'endm\\*\\*\\*\\*',
-  'gamer\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*',
-  'asegeio\\*\\*\\*\\*\\*\\*\\*\\*\\*',
-  'whys\\*\\*\\*\\*\\*\\*\\*',
-  'Brot\\*\\*\\*\\*\\*\\*',
-  'imwith\\*\\*\\*\\*\\*\\*\\*',
-  'lucky-you-got-this-password-lol-nocencor',
-  'starb\\*\\*\\*\\*\\*\\*\\*',
-  'egghunt2\\*\\*\\*',
-  'secr\\*\\*\\*\\*\\*'
+  'DeezTastyNuts',
+  'i<3redditmodsUWU',
+  'yh-loves-h',
+  'AmBestDiscordModEver',
+  'BananaTastesAwful',
+  'bread_gud',
+  'fuckiforgotmypassword',
+  'GetRektHackers',
+  'ummmmmmmmmm',
+  'password',
+  'VERYVERYVERYsecurePassword',
+  'youtubewhereisdislikebutton',
+  'ihadastroke',
+  'YAMETEONIICHAN~',
+  'WTFisApassword',
+  'ZAWARUDO',
+  'secretlyLikeCandice',
+  'pinkyFeb04'
 ]
 
 const ips = [
-  '10.313.523.502.00.1',
-  '25.537.753.462.29.2',
-  '21.175.866.974.07.08',
-  '32.653.587.825.35.5',
-  '12.172.764.781.22.8',
-  '91.723.242.452.09.3',
-  '92.743.116.896.85.6',
-  '84.091.000.853.54.7',
-  '51.071.124.129.12.0'
+  '96.213.98.110.51',
+  '118.188.254.78.186',
+  '54.166.211.54.214',
+  '39.81.36.61.185',
+  '111.27.193.111.227',
+  '248.86.159.105.207',
+  '201.232.152.210.248',
+  '12.153.62.5.231',
+  '153.228.80.222.213',
+  '130.212.26.243.156',
+  '69.69.69.69.69'
 ]
 
 /**
@@ -72,13 +74,27 @@ const wait = sec => new Promise(res => setTimeout(res, sec * 1000))
 
 module.exports = {
   name: 'hack',
-  description: "Hack some 8yo's account",
+  description: "Hack some 8yo's account. 20% fail rate.",
   args: true,
   usage: '<user>',
   async execute (message, args) {
     try {
-      const { channel } = message
+      const { author, channel } = message
       const target = await getUserFromMessage(message, args[0])
+
+      // why tf are you hacking yourself
+      if (target.id === author.id) {
+        return message.reply(
+          ':x: Why tf are you hacking yourself are you stupid ._.'
+        )
+      }
+
+      // 20% fail rate
+      if (Math.random() < 0.2) {
+        return message.reply(
+          ":x: Hacking failed. You've been found out by the FBI. Run while you can."
+        )
+      }
 
       // Random fake email password and IP address
       const domain = rand(domains)
@@ -86,30 +102,40 @@ module.exports = {
       const ip = rand(ips)
 
       // Hack sequence
-      message.reply(`Hacking ${target}...`)
-      const msg = await channel.send('Status: □□□□□□□□□□ 3%: Hacking Email...')
+
+      let text = `Hacking ${target}...`
+      const msg = await message.reply(text)
+
+      const editMsg = async str => {
+        text = `${text}\n${str}`
+        return msg.edit(text)
+      }
+
+      await editMsg('Status: □□□□□□□□□□ 3%: Hacking Email...')
       await wait(1.5)
-      msg.edit(`Status: ■□□□□□□□□□ 9%: Hacked email\nEmail: ${target.username}@${domain}\nPassword: ${password}`)
-      await wait(2.3)
-      msg.edit('Status: ■■□□□□□□□□ 12%: Turning off the antivirus...')
-      await wait(1.4)
-      msg.edit('Status: ■■■□□□□□□□ 24%: Logging in to the Email...')
-      await wait(1.7)
-      msg.edit('Status: ■■■■□□□□□□ 36%: Downloading SYNAPSE X...')
-      await wait(1.3)
-      msg.edit('Status: ■■■■■□□□□□ 47%: Deleting Captcha...')
-      await wait(1.6)
-      msg.edit('Status: ■■■■■■□□□□ 54%: Deleting Paypal account...')
-      await wait(1.46)
-      msg.edit('Status: ■■■■■■■□□□ 69%: Email password changed')
+      editMsg(
+        `Status: ■□□□□□□□□□ 9%: Hacked email\nEmail: ${target.username}@${domain}\nPassword: ${password}`
+      )
       await wait(3)
-      msg.edit(`Status: ■■■■■■■■□□ 79%: IP address found: ${ip}`)
+      editMsg('Status: ■■□□□□□□□□ 12%: Turning off the antivirus...')
+      await wait(1.4)
+      editMsg('Status: ■■■□□□□□□□ 24%: Logging in to the Email...')
+      await wait(1.7)
+      editMsg('Status: ■■■■□□□□□□ 36%: Downloading SYNAPSE X...')
+      await wait(2.5)
+      editMsg('Status: ■■■■■□□□□□ 47%: Deleting Captcha...')
+      await wait(1.6)
+      editMsg('Status: ■■■■■■□□□□ 54%: Deleting Paypal account...')
+      await wait(1.46)
+      editMsg('Status: ■■■■■■■□□□ 69%: Email password changed')
+      await wait(3)
+      editMsg(`Status: ■■■■■■■■□□ 79%: IP address found: ${ip}`)
       await wait(1.8)
-      msg.edit('Status: ■■■■■■■■■□ 85%: Starting DDoS attack on IP address...')
-      await wait(1.2)
-      msg.edit('Status: ■■■■■■■■■■ 97%: Almost there...')
+      editMsg('Status: ■■■■■■■■■□ 85%: Starting DDoS attack on IP address...')
+      await wait(3.5)
+      editMsg('Status: ■■■■■■■■■■ 97%: Almost there...')
       await wait(2)
-      await msg.edit('Status: ■■■■■■■■■■ 100%: Completed')
+      await editMsg('Status: ■■■■■■■■■■ 100%: Completed')
 
       // Hacking result
       const embed = new MessageEmbed()
@@ -121,7 +147,7 @@ module.exports = {
         .addField('IP address', ip)
         .setFooter("Wow you hacked some 8yo's account, you happy now?")
         .setColor(color)
-      
+
       channel.send({ embeds: [embed] })
     } catch (error) {
       message.client.log.error(error)
