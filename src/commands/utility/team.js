094c80eccfdcc606335a76e3fa4b2cd98fcd1391
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-14 11:58:20 +0800

    reformat code

--- src/commands/utility/team.js
+++ src/commands/utility/team.js
@@ -1,8 +1,9 @@
 const config = require('../../config')
 
-const { avatar, color, url } = config.embed
+const { avatar, color } = config.embed
 
 const shuffleArray = arr => {
+  arr = [...arr]
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[arr[i], arr[j]] = [arr[j], arr[i]]
@@ -15,7 +16,9 @@ const getTeams = (players, n = 1) => {
   // for some reason you can't do
   // Array(n).fill([])
   // because it references the same array
-  const teams = Array(n).fill(1).map(() => [])
+  const teams = Array(n)
+    .fill(1)
+    .map(() => [])
   let i = 0
   while (players.length) {
     teams[i].push(players.shift())
@@ -30,7 +33,7 @@ module.exports = {
   alias: ['teams', 'teammate', 'teammates'],
   description: 'Let Phobos choose your teammates!',
   args: true,
-  usage: '<teamNumber> <player1> <player2>...',
+  usage: '<number of teams> <player1> <player2>...',
   async execute (message, args) {
     const teamNumber = parseInt(args.shift())
 
@@ -43,20 +46,18 @@ module.exports = {
     const players = shuffleArray([...args])
     const teams = getTeams(players, teamNumber)
 
-    message.reply({
-      embeds: [
-        {
-          title: 'Team Distribution',
-          description: teams
-            .map((team, index) => `**Team ${index + 1}:** ${team.join(', ')}`)
-            .join('\n'),
-          color,
-          footer: {
-            text: 'Have fun :)',
-            icon_url: avatar
-          }
-        }
-      ]
-    })
+    const embed = {
+      title: 'Team Distribution',
+      description: teams
+        .map((team, index) => `**Team ${index + 1}:** ${team.join(', ')}`)
+        .join('\n'),
+      color,
+      footer: {
+        text: 'Have fun :)',
+        icon_url: avatar
+      }
+    }
+
+    message.reply({ embeds: [embed] })
   }
 }
