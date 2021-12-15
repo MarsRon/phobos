215ec3549799b51d1e2f74a7e8b5ac339a7cf344
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-15 14:45:45 +0800

    Add bobross command

---
+++ src/commands/images/bobross.js
@@ -0,0 +1,18 @@
+const { MessageAttachment } = require('discord.js')
+const { getUserFromMessage } = require('../../utils')
+const { Bobross } = require('discord-image-generation')
+
+module.exports = {
+  name: 'bobross',
+  description: 'Bobross is painting something amazing!',
+  usage: '[user]',
+  cooldown: 5,
+  async execute (message, args) {
+    const user = await getUserFromMessage(message, args[0])
+    const avatar = user.displayAvatarURL({ format: 'png', size: 256 })
+    const bobross = await new Bobross().getImage(avatar)
+    message.reply({
+      files: [new MessageAttachment(bobross, `${user.username}-bobross.png`)]
+    })
+  }
+}
