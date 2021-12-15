acbbdaff007b958b3c4a078890a3fd2e19364216
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-15 16:55:07 +0800

    pp only uses author

--- src/commands/fun/pp.js
+++ src/commands/fun/pp.js
@@ -1,4 +1,3 @@
-const { getUserFromMessage } = require('../../utils')
 const config = require('../../config')
 
 const { color } = config.embed
@@ -7,14 +6,12 @@ module.exports = {
   name: 'pp',
   alias: ['penis', '8=D', 'ppsize'],
   description: 'Haha pp go brrr',
-  usage: '[user]',
-  async execute (message, args) {
-    const user = await getUserFromMessage(message, args[0])
-    const ppSize = Math.floor(Math.random() * 25)
+  async execute (message) {
+    const ppSize = Math.floor(Math.random() * 26)
     const pp = `8${'='.repeat(ppSize)}D`
     const embed = {
       title: 'PP Size Calculator',
-      description: `${user}'s PP is ${ppSize} banana${
+      description: `Your PP is ${ppSize} banana${
         ppSize === 1 ? '  ' : 's'
       } long!\n\`\`\`${pp}\`\`\``,
       color
