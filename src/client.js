1c92aa74f74a2970c5d64ad602c9b27cf703496a
MarsRon <71264052+MarsRon@users.noreply.github.com>
2023-04-08 23:42:09 +0800

    Fix discord.js v14 Enum breaking changes

--- src/client.js
+++ src/client.js
@@ -1,4 +1,10 @@
-const { Client, Collection, Intents, Options } = require('discord.js')
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Partials,
+  Options
+} = require('discord.js')
 
 // Required handlers
 const log = require('./handlers/logger')
@@ -78,25 +84,25 @@ class PhobosClient extends Client {
 }
 
 const options = {
-  intents: new Intents(
-    // 0 DIRECT_MESSAGE_TYPING
-    // 1 DIRECT_MESSAGE_REACTIONS
-    // 1 DIRECT_MESSAGES
-    // 0 GUILD_MESSAGE_TYPING
-    // 1 GUILD_MESSAGE_REACTIONS
-    // 1 GUILD_MESSAGES
-    // 0 GUILD_PRESENCES
-    // 1 GUILD_VOICE_STATES
-    // 0 GUILD_INVITES
-    // 0 GUILD_WEBHOOKS
-    // 1 GUILD_INTEGRATIONS
-    // 1 GUILD_EMOJIS
-    // 1 GUILD_BANS
-    // 1 GUILD_MEMBERS
-    // 1 GUILDS
-    0b011011010011111
-  ),
-  partials: ['CHANNEL', 'MESSAGE', 'REACTION'],
+  intents: [
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.Guilds
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.Channel,
+    Partials.User,
+    Partials.Reaction
+  ],
   makeCache: Options.cacheWithLimits({
     MessageManager: 100
   })
