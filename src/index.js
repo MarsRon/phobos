dcd6250e42263b53386c251179995cb5a24134df
MarsRon <71264052+MarsRon@users.noreply.github.com>
2023-12-10 14:58:04 +0800

    Add wait for internet connection before starting bot

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ const path = require('path')
 // Configure environment variables
 dotenv.config()
 
-const client = require('./client')
+// Wait for internet connection
+import('is-online').then(({ default: isOnline }) =>
+  isOnline({ timeout: 60 * 1000 })
+).then(() => {
 
-// Require handlers
-const handlers = readdirSync(path.join(__dirname, './handlers'))
-handlers.forEach(handler => require(`./handlers/${handler}`))
+  const client = require('./client')
 
-client.log.info('All handlers are loaded')
+  // Require handlers
+  const handlers = readdirSync(path.join(__dirname, './handlers'))
+  handlers.forEach(handler => require(`./handlers/${handler}`))
 
-client.login(process.env.DISCORD_TOKEN)
+  client.log.info('All handlers are loaded')
+
+  client.login(process.env.DISCORD_TOKEN)
+})
