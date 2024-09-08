d3abd8c370ac57b33f1d283d95abbc3f3aefe06d
MarsRon <71264052+MarsRon@users.noreply.github.com>
2024-09-08 18:49:07 +0800

    Fix bot not leaving on music disconnect

--- src/commands/music/disconnect.js
+++ src/commands/music/disconnect.js
@@ -5,12 +5,14 @@ module.exports = {
   guildOnly: true,
   async execute (message) {
     const { distube } = message.client
-    if (!distube.getQueue(message)) {
+    const queue = distube.getQueue(message)
+    if (!queue) {
       return message.reply(
         `:x: **I am not playing music. Use** \`${message.prefix}play\`** to play some music!**`
       )
     }
-    distube.stop(message)
+    queue.stop()
+    queue.voice.leave()
     message.reply('**Successfully disconnected**')
   }
 }
