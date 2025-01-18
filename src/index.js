bc49d5fe7c325c81df4c519305fd46a7b7535c30
MarsRon <71264052+MarsRon@users.noreply.github.com>
2025-01-18 23:49:08 +0800

    fix: Fix "No playable format found“

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ dotenv.config()
 // Fixes "403 error" bug from ytdl-core
 // https://github.com/distubejs/ytdl-core/issues/84#issuecomment-2295590460
 // https://discord.com/channels/732254550689316914/1271765204253933609/1275273097120841781
-const fs = require('fs/promises')
-const buggedFilePath = './node_modules/@distube/youtube/dist/index.js'
-fs.readFile(buggedFilePath)
-  .then(data =>
-    data.toString().replace(
-      /const info = await import_ytdl_core.default.getInfo\(song.url, this.ytdlOptions\);/g,
-      `let info = await import_ytdl_core.default.getInfo(song.url, this.ytdlOptions);\n    info.formats = info.formats.filter(f => f.hasAudio && f.url.includes("c=IOS")) /* 403 Error Workaround */`
-    )
-  ).then(data => fs.writeFile(buggedFilePath, data))
-  .then(() => console.log(`Fixed "403 error" bug from ytdl-core (see src/index.js:10)`))
-  .catch(err => console.error(err))
+// const fs = require('fs/promises')
+// const buggedFilePath = './node_modules/@distube/youtube/dist/index.js'
+// fs.readFile(buggedFilePath)
+//   .then(data =>
+//     data.toString().replace(
+//       /const info = await import_ytdl_core.default.getInfo\(song.url, this.ytdlOptions\);/g,
+//       `let info = await import_ytdl_core.default.getInfo(song.url, this.ytdlOptions);\n    info.formats = info.formats.filter(f => f.hasAudio && f.url.includes("c=IOS")) /* 403 Error Workaround */`
+//     )
+//   ).then(data => fs.writeFile(buggedFilePath, data))
+//   .then(() => console.log(`Fixed "403 error" bug from ytdl-core (see src/index.js:10)`))
+//   .catch(err => console.error(err))
 
 // Wait for internet connection
 import('is-online').then(({ default: isOnline }) =>
