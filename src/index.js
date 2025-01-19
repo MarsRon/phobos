9d4b75e76fad2068ed5a8bf529b370c04e5cfb99
MarsRon <71264052+MarsRon@users.noreply.github.com>
2025-01-19 14:26:40 +0800

    fix: Implement "Fixes for 403 errors (by fixing clients)" patch

--- src/index.js
+++ src/index.js
@@ -7,25 +7,28 @@ const path = require('path')
 // Configure environment variables
 dotenv.config()
 
-// Fixes "403 error" bug from ytdl-core
-// https://github.com/distubejs/ytdl-core/issues/84#issuecomment-2295590460
-// https://discord.com/channels/732254550689316914/1271765204253933609/1275273097120841781
-// const fs = require('fs/promises')
-// const buggedFilePath = './node_modules/@distube/youtube/dist/index.js'
-// fs.readFile(buggedFilePath)
-//   .then(data =>
-//     data.toString().replace(
-//       /const info = await import_ytdl_core.default.getInfo\(song.url, this.ytdlOptions\);/g,
-//       `let info = await import_ytdl_core.default.getInfo(song.url, this.ytdlOptions);\n    info.formats = info.formats.filter(f => f.hasAudio && f.url.includes("c=IOS")) /* 403 Error Workaround */`
-//     )
-//   ).then(data => fs.writeFile(buggedFilePath, data))
-//   .then(() => console.log(`Fixed "403 error" bug from ytdl-core (see src/index.js:10)`))
-//   .catch(err => console.error(err))
+// Implement "Fixes for 403 errors (by fixing clients)" patch
+// https://github.com/distubejs/ytdl-core/pull/163
+const fs = require('fs')
+const { Readable } = require('stream')
+const { finished } = require('stream/promises')
+const patchBaseUrl = 'https://raw.githubusercontent.com/ToddyTheNoobDud/ytdl-core-stuff/37acb1c7ca203bb33c1219bffe5fb2b820b52f69/'
+const buggedInfo = './node_modules/@distube/ytdl-core/lib/info.js'
+const buggegUtils = './node_modules/@distube/ytdl-core/lib/utils.js'
+const patchedInfo = patchBaseUrl + 'lib/info.js'
+const patchedUtils = patchBaseUrl + 'lib/utils.js'
+const patch = async (buggedFile, patchUrl) => {
+  const stream = fs.createWriteStream(buggedFile)
+  const { body } = await fetch(patchUrl)
+  return finished(Readable.fromWeb(body).pipe(stream))
+}
 
 // Wait for internet connection
 import('is-online').then(({ default: isOnline }) =>
   isOnline({ timeout: 60 * 1000 })
-).then(() => {
+).then(async () => {
+  await Promise.all([patch(buggedInfo, patchedInfo), patch(buggegUtils, patchedUtils)])
+  .then(() => console.log(`Implement "Fixes for 403 errors (by fixing clients)" patch from ytdl-core (see src/index.js:10)`))
 
   const client = require('./client')
 
